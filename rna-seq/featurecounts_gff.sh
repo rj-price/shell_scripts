2203269d8011f03545cac3f97851adf1feb616ea
Jordan Price <rj_price@hotmail.co.uk>
2023-02-15 17:13:16 +0000

    update

---
+++ rna-seq/featurecounts_gff.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH -J featurecounts
+#SBATCH --partition=long
+#SBATCH --mem=500M
+#SBATCH --cpus-per-task=4
+
+# sorted bam = $1
+# transcriptome = $2
+
+export MYCONDAPATH=/mnt/shared/scratch/jnprice/apps/conda
+
+source ${MYCONDAPATH}/bin/activate rna-seq
+
+fileshort=$(basename $1 | sed s/".sorted.bam"//g)
+
+featureCounts -T 4 -g Parent -a $2 -o "$fileshort".txt $1
+
